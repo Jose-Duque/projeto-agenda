dd1c03ccb6d6b5ae83bede46b0df51ce75c03f35
José Carlos Duque Filho <cj_duque@hotmail.com>
2022-01-12 20:46:29 -0300

    ADD Paginator

--- contatos/admin.py
+++ contatos/admin.py
@@ -3,7 +3,8 @@ from .models import Categoria, Contato
 
 class ContatoAdmin(admin.ModelAdmin):
 
-     list_display = ('nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
-
+     list_display = ('nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria', 'mostar')
+     list_editable = ('telefone', 'mostar')
+     search_fields = ('nome', 'telefone')
 admin.site.register(Categoria)
 admin.site.register(Contato,ContatoAdmin)
--- contatos/views.py
+++ contatos/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Contato
+from django.core.paginator import Paginator
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos_list = Contato.objects.all()
+    paginator = Paginator(contatos_list, 5)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
 
 def show_contato(request, id_contato):
-    contato = Contato.objects.get(id=id_contato)
+    # contato = Contato.objects.get(id=id_contato)
+    contato = get_object_or_404(Contato, id=id_contato)
     return render(request, 'contatos/show_contato.html', {
         'contato': contato
     })
