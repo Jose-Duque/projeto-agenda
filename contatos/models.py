1e760fa1f8e99098bcfa9d5c99b99c8e03aa7a67
José Carlos Duque Filho <cj_duque@hotmail.com>
2022-01-12 15:56:03 -0300

    Model created Contato end Categoria

--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=30)
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=30)
+    sobrenome = models.CharField(max_length=30, blank=True)
+    telefone = models.CharField(max_length=10)
+    email = models.CharField(max_length=30, blank=True)
+    data_criacao = models.DateTimeField(default=timezone.now())
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
