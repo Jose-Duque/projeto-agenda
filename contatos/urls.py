e8058f8775880de6b89b05930267c64f6ae7781f
José Carlos Duque Filho <cj_duque@hotmail.com>
2022-01-13 09:05:06 -0300

    Campo de busca

--- contatos/urls.py
+++ contatos/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('busca/', views.busca, name='busca'),
     path('<int:id_contato>', views.show_contato, name='show_contato'),
 ]
--- contatos/views.py
+++ contatos/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, Http404
 from .models import Contato
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 def index(request):
-    contatos_list = Contato.objects.all()
-    paginator = Paginator(contatos_list, 5)
+    contatos_list = Contato.objects.order_by('nome')
+    paginator = Paginator(contatos_list, 3)
 
     page = request.GET.get('p')
     contatos = paginator.get_page(page)
@@ -16,6 +18,36 @@ def index(request):
 def show_contato(request, id_contato):
     # contato = Contato.objects.get(id=id_contato)
     contato = get_object_or_404(Contato, id=id_contato)
+
+    if not contato:
+       raise  Http404()
+
     return render(request, 'contatos/show_contato.html', {
         'contato': contato
+    })
+
+def busca(request):
+    termo = request.GET.get('termo')
+    campos = Concat('nome', Value(' '), 'sobrenome')
+    print(termo)
+
+    if not termo:
+        raise Http404
+
+    contatos_list = Contato.objects.annotate(nome_completo=campos).filter(
+        nome_completo__contains=termo
+    )
+
+    # contatos_list = Contato.objects.order_by('nome').filter(
+    #     Q(nome__contains=termo) | Q(sobrenome__contains=termo)
+    # )
+    print(contatos_list.query)
+
+    paginator = Paginator(contatos_list, 5)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
     })
